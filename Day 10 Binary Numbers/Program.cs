10ae3ec79123d0c1b81601a48495ca856ed825ed
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-01 13:57:11 +0200

    Day 10: that was very easy :)

---
+++ Day 10 Binary Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        // int n = Convert.ToInt32(Console.ReadLine().Trim());
+        int n = 13;
+        string bin = Convert.ToString(n, 2);
+        int countingOnes = 0, maxOnes = 0;
+        for (int i = 0; i < bin.Length; i++)
+        {
+            if (bin[i] == '1')
+            {
+                countingOnes++;
+                if (maxOnes < countingOnes) maxOnes = countingOnes;
+            }
+            else
+            {
+                countingOnes = 0;
+            }
+
+        }
+        Console.WriteLine(maxOnes);
+    }
+}
