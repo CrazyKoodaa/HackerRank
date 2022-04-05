a513011d15905b768bef54568a4d3c77c0b5fc18
Ravenmujitsuno <ravenmujitsuno@gmail.com>
2022-04-05 14:56:18 +0200

    Day 13 Scopes of Variables - solution

---
+++ Day 14 Scopes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+    private int[] a;
+
+
+
+    // Add your code here
+    public Difference(int[] array) => elements = array;
+    public void computeDifference()
+    {
+        maximumDifference = elements.Max() - elements.Min();
+    }
+}
+
+
+class Solution
+{
+static void Main(string[] args)
+{
+    var x = Convert.ToInt32(Console.ReadLine());
+
+    int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+    Difference d = new Difference(a);
+
+    d.computeDifference();
+
+    Console.Write(d.maximumDifference);
+}
+}
