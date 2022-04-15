7ccbf57b4751cb3313baa7869990c5d888795e1d
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-15 14:59:18 +0200

    Day 23 BST Level Order Traversion and Day 24 More linked List solutions

---
+++ Day 23 BST Level-Order Traversal/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Node
+{
+    public Node left, right;
+    public int data;
+    public Node(int data)
+    {
+        this.data = data;
+        left = right = null;
+    }
+}
+class Solution
+{
+    public static Queue<Node> qNode = new Queue<Node>();  
+    public static List<string> lNodes = new List<string>();
+
+    static void levelOrder(Node root)
+    { 
+        //Write your code here
+
+        // Queue both Childs if available    
+        if (root.left != null)
+            qNode.Enqueue(root.left);
+        if (root.right != null) 
+            qNode.Enqueue(root.right);
+        
+        // Add the Nodes data into a list for further coding
+        lNodes.Add(root.data.ToString());
+
+        // if the Queue is not yet empty, then take the first element out of the queue
+        // if the Queue is empty, then write all the strings together with a space in between
+        if (qNode.Count > 0)
+            levelOrder(qNode.Dequeue());
+        else
+            Console.WriteLine(lNodes.Aggregate((a, b) => a + ' ' + b).TrimEnd());
+
+        
+
+
+    }
+
+    static Node insert(Node root, int data)
+    {
+        if (root == null)
+        {
+            return new Node(data);
+        }
+        else
+        {
+            Node cur;
+            if (data <= root.data)
+            {
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else
+            {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+    static void Main(String[] args)
+    {
+        Node root = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            root = insert(root, data);
+        }
+        levelOrder(root);
+
+    }
+}
---
+++ Day 24 More Linked List/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+class Node
+{
+	public int data;
+	public Node next;
+	public Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+
+}
+class Solution
+{
+
+	
+	public static List<int> nodes = new List<int>();
+	public static Node removeDuplicates(Node head)
+	{
+		//Write your code here
+		if (head == null) return head;
+
+		while (head != null)
+		{
+			nodes.Add(head.data);
+			head = head.next;
+		}
+		
+		
+		Node headNew = null;
+
+		for (int i = 0; i < nodes.Count; i++)
+        {
+			//int haveToCount = nodes.Count;
+			for (int j = i + 1; j < nodes.Count; j++)
+            {
+				if (nodes[i] == nodes[j])
+                {
+					nodes.RemoveAt(j);
+					j--;
+                }
+            }
+        }
+
+		foreach (var node in nodes)
+			headNew = insert(headNew, node);
+
+		return headNew;
+		
+	}
+
+	public static Node insert(Node head, int data)
+	{
+		Node p = new Node(data);
+
+
+		if (head == null)
+			head = p;
+		else if (head.next == null)
+			head.next = p;
+		else
+		{
+			Node start = head;
+			while (start.next != null)
+				start = start.next;
+			start.next = p;
+
+		}
+		return head;
+	}
+	public static void display(Node head)
+	{
+		Node start = head;
+		while (start != null)
+		{
+			Console.Write(start.data + " ");
+			start = start.next;
+		}
+	}
+	static void Main(String[] args)
+	{
+
+		Node head = null;
+		int T = Int32.Parse(Console.ReadLine());
+		while (T-- > 0)
+		{
+			int data = Int32.Parse(Console.ReadLine());
+			head = insert(head, data);
+		}
+		head = removeDuplicates(head);
+		display(head);
+	}
+}
